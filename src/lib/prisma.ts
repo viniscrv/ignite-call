2793274821c6dd63301313326ee2c1ec4e9ccc37
Vinícius <viniscrv@gmail.com>
2022-12-27 15:25:23 -0300

    adicionado o pré-registro ao db

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-export const Prisma = new PrismaClient({
+export const prisma = new PrismaClient({
     log: ["query"],
 });
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { api } from "../../lib/axios";
+import { AxiosError } from "axios";
 
 const registerFormSchema = z.object({
   username: z
@@ -40,7 +42,16 @@ export default function Register() {
   }, [router.query?.username, setValue]);
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data);
+    try {
+      await api.post("/users", {
+        username: data.username,
+        name: data.name,
+      })
+    } catch(err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        return alert(err.response.data.message);
+      }
+    }
   }
 
   return (
