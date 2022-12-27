6b7727a6881a882e5e333bc3666b9d7b38cee96d
Vinícius <viniscrv@gmail.com>
2022-12-27 00:39:42 -0300

    criado página de registro

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -4,6 +4,7 @@ import { Form, FormAnnotation } from "./styles";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { useRouter } from "next/router";
 
 const claimUsernameSchema = z.object({
   username: z
@@ -21,13 +22,17 @@ export function ClaimUsernameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameForm>({
     resolver: zodResolver(claimUsernameSchema),
   });
 
+  const router = useRouter();
+
   async function handleClaimUsername(data: ClaimUsernameForm) {
-    console.log(data.username);
+    const { username } = data;
+
+    await router.push(`/register?username=${username}`);
   }
 
   return (
@@ -39,7 +44,7 @@ export function ClaimUsernameForm() {
           placeholder="seu-usuario"
           {...register("username")}
         />
-        <Button size="sm" type="submit">
+        <Button size="sm" type="submit" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
