e72f675f80c81c3312e9e7d6546355d63bfcf0e0
Vinícius <viniscrv@gmail.com>
2023-01-04 15:08:05 -0300

    convertido horarios de entrada com Zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -10,6 +10,7 @@ import {
 import { ArrowRight } from "phosphor-react";
 import { Controller, useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
+import { convertTimeStringToMinutes } from "../../../utils/conver-time-string-to-minutes";
 import { getWeekDays } from "../../../utils/get-week-days";
 import { Container, Header } from "../styles";
 import {
@@ -35,10 +36,25 @@ const timeIntervalsFormSchema = z.object({
     .transform((intervals) => intervals.filter((interval) => interval.enabled))
     .refine(intervals => intervals.length > 0, {
       message: "Você precisa selecionar pelo menos um dia da semana!"
-    }),
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(intervals => {
+      return intervals.every(interval => interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes)
+    }, { 
+      message: "O horário de término deve ser pelo menos uma hora distante do início."  
+    })
 });
 
-type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
 
 export default function TimeIntervals() {
   const {
@@ -47,7 +63,7 @@ export default function TimeIntervals() {
     control,
     formState: { isSubmitting, errors },
     watch,
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
     defaultValues: {
       intervals: [
         { weekDay: 0, enabled: false, startTime: "08:00", endTime: "18:00" },
@@ -71,8 +87,10 @@ export default function TimeIntervals() {
 
   const intervals = watch("intervals");
 
-  function handleSetTimeIntervals(data: TimeIntervalsFormData) {
-    console.log(data);
+  function handleSetTimeIntervals(data: any) {
+    const formData = data as TimeIntervalsFormOutput;
+
+    console.log(formData);
   }
 
   return (
