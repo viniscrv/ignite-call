386b4b01e2a75bb7fa807c474eec133e34acfedf
Vinícius <viniscrv@gmail.com>
2023-01-10 17:37:55 -0300

    adicionado condicional para dias selecionados

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -14,7 +14,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[];
 
-export function Calendar() {
+interface CalendarProps {
+    selectedDate?: Date | null;
+    onDateSelected: (date: Date) => void;
+}
+
+export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
 
     const [currentDate, setCurrentDate] = useState(() => {
         return dayjs().set("date", 1)
@@ -66,7 +71,7 @@ export function Calendar() {
                 return { date, disabled: true }
             }),
             ...daysInMonthArray.map(date => {
-                return { date, disabled: false }
+                return { date, disabled: date.endOf("day").isBefore(new Date()) }
             }),
             ...nextMonthFillArray.map(date => {
                 return { date, disabled: true }
@@ -84,7 +89,7 @@ export function Calendar() {
             }
 
             return weeks;
-        }, [])
+        }, []);
 
         return calendarWeeks;
     }, [currentDate]);
@@ -121,7 +126,7 @@ export function Calendar() {
                                 {days.map(({ date, disabled }) => {
                                     return (
                                         <td key={date.toString()}>
-                                            <CalendarDay disabled={disabled}>{date.get("date")}</CalendarDay>
+                                            <CalendarDay onClick={() => onDateSelected(date.toDate())} disabled={disabled}>{date.get("date")}</CalendarDay>
                                         </td>
                                     )
                                 })}
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { Calendar } from "../../../../../components/Calendar";
 import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
 
 export function CalendarStep() {
 
-    const isDateSelected = false;
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+    const isDateSelected = !!selectedDate;
 
     return (
         <Container isTimePickerOpen={isDateSelected}>
-            <Calendar /> 
+            <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate}/> 
 
             {isDateSelected && (
                 <TimePicker>
@@ -15,19 +18,16 @@ export function CalendarStep() {
 
                     <TimePickerList>
                         <TimePickerItem>08:00h</TimePickerItem>
-                        <TimePickerItem>08:00h</TimePickerItem>
-                        <TimePickerItem>08:00h</TimePickerItem>
-                        <TimePickerItem>08:00h</TimePickerItem>
                         <TimePickerItem>09:00h</TimePickerItem>
-                        <TimePickerItem>08:00h</TimePickerItem>
+                        <TimePickerItem>10:00h</TimePickerItem>
+                        <TimePickerItem>11:00h</TimePickerItem>
                         <TimePickerItem>12:00h</TimePickerItem>
-                        <TimePickerItem>08:00h</TimePickerItem>
-                        <TimePickerItem>08:00h</TimePickerItem>
                         <TimePickerItem>13:00h</TimePickerItem>
-                        <TimePickerItem>08:00h</TimePickerItem>
-                        <TimePickerItem>19:00h</TimePickerItem>
-                        <TimePickerItem>08:00h</TimePickerItem>
-                        <TimePickerItem>23:00h</TimePickerItem>
+                        <TimePickerItem>14:00h</TimePickerItem>
+                        <TimePickerItem>15:00h</TimePickerItem>
+                        <TimePickerItem>16:00h</TimePickerItem>
+                        <TimePickerItem>17:00h</TimePickerItem>
+                        <TimePickerItem>18:00h</TimePickerItem>
                     </TimePickerList>
                 </TimePicker>
             )}
