0b402ea8d196a41ec7bf4153541c7d30741f4c3d
Vinícius <viniscrv@gmail.com>
2023-02-15 00:43:18 -0300

    criado rota de agendamento

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,9 +1,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
 import dayjs from "dayjs";
+import { useRouter } from "next/router";
 import { CalendarBlank, Clock } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { api } from "../../../../../lib/axios";
 import { ConfirmForm, FormActions, FormError, FormHeader } from "./styles";
 
 const confirmFormSchema = z.object({
@@ -25,9 +27,22 @@ export function ConfirmStep({schedulingDate, onCancelConfirmation}: ConfirmStepP
         resolver: zodResolver(confirmFormSchema)
     });
 
+    const router = useRouter();
 
-    function handleConfirmScheduling(data: ConfirmFormData) {
-        console.log(data);
+    const username = String(router.query.username);
+
+    async function handleConfirmScheduling(data: ConfirmFormData) {
+
+        const { name, email, observations } = data;
+
+        await api.post(`/users/${username}/schedule`, {
+            name,
+            email,
+            observations,
+            date: schedulingDate,
+        });
+
+        onCancelConfirmation();
     }
 
     const describedDate = dayjs(schedulingDate).format("DD[ de ]MMMM[ de ]YYYY");
